cb37dc1e7db542fb912714a4d7db5b004c6309df
Johan Commelin <johan@commelin.net>
2018-06-28 09:49:23 +0200

    Improve readability

--- src/valuations.lean
+++ src/valuations.lean
@@ -336,19 +336,24 @@ structure valuations (R : Type) [comm_ring R] :=
 
 attribute [instance] valuations.Hα
 
---instance (R : Type) [comm_ring R] : has_coe_to_fun (valuations R) :=
---{ F := λ v,R → option v.α, 
---  coe := λ v,v.f
---}
+namespace valuations
+
+variables (R : Type) [comm_ring R] {α : Type} [Hα : linear_ordered_comm_group α]
+
+instance : has_coe_to_fun (valuations R) :=
+{ F := λ v, _,
+  coe := λ v, v.f }
 
 /- Wedhorn 1.27 (ii) -/
-instance valuations.setoid (R : Type) [comm_ring R] : setoid (valuations R) :=
-{ r := λ v w, ∀ r s : R, v.f r ≤ v.f s ↔ w.f r ≤ w.f s,
+instance : setoid (valuations R) :=
+{ r := λ v w, ∀ r s : R, v(r) ≤ v(s) ↔ w(r) ≤ w(s),
   iseqv := ⟨
     -- reflexivity 
-    λ _ _ _,iff.rfl,
+    λ _ _ _, iff.rfl,
     -- symmetry
-    λ v w H r s,(H r s).symm,
+    λ v w H r s, (H r s).symm,
     -- transitivity
-    λ v w x Hvw Hwx r s,iff.trans (Hvw r s) (Hwx r s)⟩
+    λ v w x Hvw Hwx r s, iff.trans (Hvw r s) (Hwx r s)⟩
 } 
+
+end valuations
